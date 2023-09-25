46d73be220ec967ab42d9e18704f354b07ebadf0
Gemagit <gemitaphotographer@gmail.com>
2023-09-25 22:25:43 +0200

    add/index.js

---
+++ Pair programming/index.js
@@ -0,0 +1,41 @@
+let variableSinValor;
+
+let booleano1=true;
+let booleano2=false;
+
+const PI= 3.14;
+const TAU= 2 * 3.14;
+
+let booleanoAnd = booleano1 && booleano2;
+
+let booleanoNot = !booleano1;
+
+let booleanoMixO = () {
+    for ((booleano1 || booleano2) && (booleano1 || (!booleano1 & !booleano2)))
+}
+ 
+let incrementarDesp = 2 ;
+let resultadoDesp = incrementarDesp++;
+
+let incrementarAntes = 2 ;
+let resultadoAntes = incrementarAntes--;
+
+let contarHasta10_2 = 0 ;
+for (contarHasta10_2; contarHasta10_2++; contarHasta10_2==10);
+
+let postl = 0;
+let postJ = 0;
+let i = 0  ; 
+while (i<10 ) {
+    postl = postl + postJ++;
+    i++
+}
+
+
+SumaPares = 0;
+for (i = 0; i < 10; i++) {
+    if (i / 2 == 0) {
+        (SumaPares = SumaPares + i);
+    }
+}
+
---
+++ Proyecto individual/index.js
@@ -0,0 +1,44 @@
+let variableValorNumerico= 8;
+
+const MiNombre= "Gema";
+
+const MiNumeroFavorito = 7;
+
+let booleanoOr = (booleano1 || booleano2);
+
+let booleanoMix1 = (booleano1 && (TAU / 2== 3.14)||(variableValorNumerico >= MinumeroFavorito));
+
+let seisNoEsNueve = (6=9);
+
+let booleanoMix2 =( variableValorNumerico+(-0) || ( MiNumeroFa * TAU));
+
+let valorSuma= (miNumeroFav + variableValorNumerico);
+
+let valorResta= (miNumeroFavorito - variableValorNumerico);
+
+let valorMultiplicacion = MiNumeroFav* variableValorNumerico;
+
+let valorDivision= MiNumeroFavorito/ 3;
+
+let contarhasta10= (0);
+    while (contarHasta10==10) {
+        contarHasta10++;
+    }
+
+let preI= 0;
+let preJ= 0;
+    for (let i = 0; i < 11; i++) {
+    preI += ++preJ;
+}
+
+
+sumalmpares = 0;
+    while (i < 10) {
+     if (sumaPares%1){
+        sumaPares+ i;
+     }
+}
+
+
+
+
